430fa24c6c63bad95e9b1c55cd45905ad5c0c9bd
Carlos Alfaro <carlosedu007007@yahoo.com>
2017-01-01 21:29:14 -0600

    Fixed bugs

--- php/Restore.php
+++ php/Restore.php
@@ -3,9 +3,14 @@ include './Connet.php';
 $restorePoint=SGBD::limpiarCadena($_POST['restorePoint']);
 $sql=explode(";",file_get_contents($restorePoint));
 $totalErrors=0;
+set_time_limit (60);
+$con=mysqli_connect(SERVER, USER, PASS, BD);
+$con->query("SET FOREIGN_KEY_CHECKS=0");
 for($i = 0; $i < (count($sql)-1); $i++){
-    if(SGBD::sql("$sql[$i];")){  }else{ $totalErrors++; }
+    if($con->query($sql[$i].";")){  }else{ $totalErrors++; }
 }
+$con->query("SET FOREIGN_KEY_CHECKS=1");
+$con->close();
 if($totalErrors<=0){
 	echo "Restauración completada con éxito";
 }else{
